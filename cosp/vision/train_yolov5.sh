eef24b8cad16e74b80b8aee1ecb74b8861890543
Kaiyu Zheng <kaiyutony@gmail.com>
2021-07-27 23:18:23 -0400

    increase epoch

--- cosp/vision/train_yolov5.sh
+++ cosp/vision/train_yolov5.sh
@@ -1,3 +1,3 @@
 python yolov5/train.py --img 600\
-       --batch 16 --epochs 3 --data yolov5-dataset.yaml\
+       --batch 16 --epochs 10 --data yolov5-dataset.yaml\
        --weights yolov5/yolov5s.pt
