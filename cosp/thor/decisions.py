c71c7e3df9a5188632b17c8a2ebf66f35b3f0b14
Kaiyu Zheng <kaiyutony@gmail.com>
2021-08-09 17:43:05 -0400

    separate high and low levels with dedication

--- cosp/thor/decisions.py
+++ cosp/thor/decisions.py
@@ -1,9 +1,35 @@
-from ..framework import Decision
+import pomdp_py
+from ..framework import Decision, Action
+
+# # Low
+
+
+
+
+# class LowLevelTransitionModel(pomdp_py.TransitionModel):
+#     pass
+
+
+
+
+
+
 
 class MoveDecision(Decision):
     def __init__(self, dest):
         super().__init__("move-to-{}".format(dest))
         self.dest = dest
+
+    # def form_pomdp(self, ...):
+    #     transition_model = make_transition_model(robotstate, movements)
+    #     observation_model = make_observation_model(robotpose)
+    #     policy_model = make_policy_model(movements)
+    #     reward_model = reachtodestreward
+    #     init_belief = currentdist
+
+
+
+        # pass
 
 class SearchDecision(Decision):
     def __init__(self):
