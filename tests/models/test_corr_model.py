3c320254645d549adcca8d494f908a88152055e4
Kaiyu Zheng <kaiyutony@gmail.com>
2021-08-12 00:06:53 -0400

    corr model behaves as expected

--- tests/models/test_corr_model.py
+++ tests/models/test_corr_model.py
@@ -28,17 +28,12 @@ def corr_func(target_pos, object_pos,
     Returns a float value to essentially mean
     Pr(Si = object_pos | Starget = target_pos)
     """
-    # # This is a rather arbitrary function for now.
+    # This is a rather arbitrary function for now.
     if target_class == objclass:
         corr = 1.0
     else:
         corr = CORR_MATRIX[(target_class, objclass)]
     distance = euclidean_dist(target_pos, object_pos)
-    # if corr > 0:
-    #     return (1.0 - corr) / (distance + 0.2)
-    # else:
-    #     # 7.5 is a distance threshol
-    #     return (1.0 - abs(corr)) / (-distance - 7.5)
     if corr > 0:
         return distance <= 2.0
     else:
