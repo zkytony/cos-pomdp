36c499f9db3b981f5c35fea4eab82deb99191db0
Kaiyu Zheng <kaiyutony@gmail.com>
2021-08-31 20:49:16 -0400

    minor

--- tests/thor/test_cosagent_complete_informed.py
+++ tests/thor/test_cosagent_complete_informed.py
@@ -1,5 +1,9 @@
 from test_cosagent_complete import _test_complete_search
 
+def step_act_cb(task_env, agent, **kwargs):
+    if agent._goal_handler.done:
+        import pdb; pdb.set_trace()
+
 
 if __name__ == "__main__":
     _test_complete_search("Bowl", "Book",
