45d6b70249cab6be21ab1e6a0ed674d1da994029
Kaiyu Zheng <kaiyutony@gmail.com>
2021-09-06 20:54:07 -0400

    note on gt corr

--- cospomdp/utils/corr_funcs.py
+++ cospomdp/utils/corr_funcs.py
@@ -13,7 +13,8 @@ class ConditionalSpatialCorrelation:
     distances of instances of si and starget.
     """
 
-    def __init__(self, target, other, distances, sigma=0.1):
+    def __init__(self, target, other, distances,
+                 sigma=0.1, for_gt=False):
         """
         target (ID, class): the target object
         other (ID, class): the other object
@@ -21,12 +22,15 @@ class ConditionalSpatialCorrelation:
         self.target = target
         self.other = other
 
-        if len(distances) == 1:
-            self._gaussian = pomdp_py.Gaussian([distances[0]],
-                                               [sigma**2])
-        else:
-            self._gaussian = pomdp_py.Gaussian([np.mean(distances)],
-                                               [np.var(distances)])
+        if not for_gt:
+            if len(distances) == 1:
+                self._gaussian = pomdp_py.Gaussian([distances[0]],
+                                                   [sigma**2])
+            else:
+                self._gaussian = pomdp_py.Gaussian([np.mean(distances)],
+                                                   [np.var(distances)])
+
+
 
     def func(self, target_loc, other_loc, target_id, other_id):
         if target_id != self.target[0]:
@@ -35,3 +39,15 @@ class ConditionalSpatialCorrelation:
             raise ValueError(f"unexpected other id {other_id}")
         dist = [euclidean_dist(target_loc, other_loc)]
         return self._gaussian[dist]
+
+    def gt_func(self, target_loc, other_loc, target_id, other_id):
+        """
+        Used for the "groundtruth" correlation baselines.
+
+        Here is what I mean by "groundtruth correlation"
+        Pr(si | starget) returns 1.0 (or with some little noise, gaussian noise?)
+        if si is the **True** target state, regardless
+        of starget's value. Basically, as long as you observe the correlated object,
+        your belief will be updated so that it'll be highest on the true target location.
+        This is the best correlation can ever do.
+        """
