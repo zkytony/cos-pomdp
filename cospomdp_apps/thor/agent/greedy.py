ccd72b7f00e79dca764fbb4212f546e8f83b7f97
Kaiyu Zheng <kaiyutony@gmail.com>
2021-09-07 09:35:21 -0400

    implemented particle filters for greedy agent

--- cospomdp_apps/thor/agent/greedy.py
+++ cospomdp_apps/thor/agent/greedy.py
@@ -17,9 +17,11 @@ searching for the target object based on the maintained belief.
 (We do not do this but do something along a similar line_
 """
 import pomdp_py
+import random
 from ..common import TOS_Action, ThorAgent
 from .cospomdp_basic import ThorObjectSearchCosAgent, GridMapSearchRegion
 from cospomdp.models.agent import build_cos_observation_model
+from cospomdp.domain.state import CosState
 
 class GreedyNbvAgent:
     """Greedy next-best-view agent.
@@ -54,7 +56,7 @@ class GreedyNbvAgent:
     Pr(si' | B, a, z) = Pr(si' | starget', B, a, z) Pr(starget' | B, a, z)
                       = Pr(si' | starget') Pr(starget' | B, a, z)
     """
-    def __init__(self, robot_id, target, search_region, corr_dists, detectors,
+    def __init__(self, target, init_robot_state, search_region, corr_dists, detectors,
                  num_particles=100, prior={}):
         """
         prior: Maps from object_id to a map from search region location to a float.
@@ -64,40 +66,120 @@ class GreedyNbvAgent:
             Does not need to contain an entry for the target object
         """
         self.search_region = search_region
-        self.robot_id = robot_id
+        self._init_robot_state = init_robot_state
         self.target = target
+        self.brobot = pomdp_py.WeightedParticles([(self._init_robot_state, 1.0)])
         self.particle_beliefs = {}
+        self._num_particles = num_particles
         # initialize particle beliefs.
         for objid in detectors:
-            self.particle_beliefs[objid] = self._init_belief(objid, num_particles, prior=prior.get(objid, {}))
+            self.particle_beliefs[objid] = self._init_belief(objid, prior=prior.get(objid, {}))
 
         # Constructs the CosObservationModel
         self.observation_model = build_cos_observation_model(
-            corr_dists, detectors, robot_id, target[0])
+            corr_dists, detectors, self.robot_id, target[0])
+
+    @property
+    def robot_id(self):
+        return self._init_robot_state.id
+
+    @property
+    def target_id(self):
+        return self.target[0]
 
     @property
     def detectable_objects(self):
         return list(self.detectors.keys())
 
-    def _init_belief(self, objid, num_particles, prior={}):
+    def _init_belief(self, objid, prior={}):
         """prior: Maps from search region location to a float."""
         # For every detectable object, maintain a set of particle beliefs
         # The initial belief is
         if len(prior) > 0:
             hist = pomdp_py.Histogram(prior)
         else:
-            hist = pomdp_py.Histogram({loc:1.0} for loc in self.search_region)
+            hist = None
         particles = []
         for objid in self.detectable_objects:
             objclass = self.detectable_objects[objid][1]
-            for i in range(num_particles):
-                loc = hist.random()
+            for i in range(self._num_particles):
+                if hist is not None:
+                    loc = hist.random()
+                else:
+                    loc = random.sample(self.search_region.locations, 1)[0]
+
                 weight = hist[loc]
                 si = self.search_region.object_state(
                     objid, objclass, loc
                 )
                 particles.append((si, weight))
-        return particles
+        return pomdp_py.WeightedParticles(particles)
+
+    def update_belief(self, observation):
+        """
+        Args:
+            observation (CosObservation)
+        """
+        robotobz = observation.z(self.robot_id)
+        rstate_class = self._init_robot_state.__class__
+        next_srobot = rstate_class.from_obz(robotobz)
+        next_brobot = pomdp_py.WeightedParticles([(next_srobot, 1.0)])
+        self.brobot = next_brobot
+
+        btarget = self.particle_beliefs[self.target_id]
+        next_btarget = self._update_particle_belief(btarget, observation, is_target=True)
+        new_particle_beliefs = {self.target_id : next_btarget}
+        for objid in self.particle_beliefs:
+            if objid != self.targte_id:
+                bobj = self.particle_beliefs[objid]
+                next_bobj = self._update_object_particles(objid, bobj, observation,
+                                                          next_btarget, next_srobot,
+                                                          self._num_particles)
+                new_particle_beliefs[objid] = next_bobj
+        self.particle_beliefs = new_particle_beliefs
+
+    def _update_target_particles(self, btarget, next_srobot, observation, num_particles):
+        _temp_particles = []
+        for i in range(num_particles):
+            starget = btarget.random()
+            next_state = CosState({self.target_id: starget,
+                                   next_srobot.id: next_srobot})
+            weight = self.observation_model.probability(observation, next_state)
+            _temp_particles.append((starget, weight))
+        # resampling
+        _temp_particles = pomdp_py.WeightedParticles(_temp_particles)
+        resampled_particles = []
+        for i in range(num_particles):
+            starget = _temp_particles.random()
+            weight = _temp_particles[starget]
+            resampled_particles.append((starget, weight))
+        return pomdp_py.WeightedParticles(resampled_particles)
+
+    def _update_object_particles(self, objid, bobj, observation, next_btarget, next_srobot, num_particles):
+        """Particle Filter that follows
+        Pr(si' | B, a, z) = Pr(si' | starget', B, a, z) Pr(starget' | B, a, z)
+                         ~= Pr(si' | starget', B, a, zi) Pr(starget' | B, a, z)
+                          = Pr(zi | si', starget') Pr(si' | starget', B) Pr(starget' | B, a, z)
+                          = Pr(zi | si') B(si') * Pr(si' | starget') Pr(starget' | B, a, z)
+
+        Note that Pr(starget' | B, a, z) has been updated already."""
+        _temp_particles = []
+        for i in range(num_particles):
+            starget = next_btarget.random()
+            dist_si = self.observation_model.z(objid).corr_cond_dist(starget)
+            si = dist_si.sample()
+            zi = observation.z(objid)
+            zi_prob = self.observation_model.z(objid).detection_model.probability(zi, si, next_srobot)
+            weight = zi_prob
+            _temp_particles.append((si, weight))
+        _temp_particles = pomdp_py.WeightedParticles(_temp_particles)
+        resampled_particles = []
+        for i in range(num_particles):
+            si = _temp_particles.random()
+            weight = _temp_particles[starget]
+            resampled_particles.append((si, weight))
+        return pomdp_py.WeightedParticles(resampled_particles)
+
 
 class ThorObjectSearchGreedyNbvAgent(ThorAgent):
     """Uses a GreedyNbvAgent to search in thor."""
