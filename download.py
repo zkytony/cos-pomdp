e2e3277c9d5674dc458ecaeb04212aad875fb8dc
Kaiyu Zheng <kaiyutony@gmail.com>
2021-09-09 22:54:40 -0400

    download data script

---
+++ download.py
@@ -0,0 +1,66 @@
+import os
+import gdown
+import subprocess
+import yaml
+import pathlib
+ABS_PATH = os.path.dirname(os.path.abspath(__file__))
+
+outfname = "yolov5-training-data.zip"
+output = f"data/{outfname}"
+if not os.path.exists(output):
+    print("Downloading yolov5 training data")
+    yolov5_training_data_url = "https://drive.google.com/uc?id=1V0GC3wyTsrEAaDfXR0ZyGWLY4j2hYMsx"
+    gdown.download(yolov5_training_data_url, output, quiet=False)
+    cmd=f'''
+cd data/
+unzip {outfname}
+cd ..
+'''
+    subprocess.check_output(cmd, shell=True)
+else:
+    print(f"{output} already exists")
+
+# Go into each dataset file, update the yaml file's path
+for name in os.listdir("data"):
+    if name.startswith("yolo"):
+        config_file = f"data/{name}/{name}-dataset.yaml"
+        if not os.path.exists(config_file):
+            continue
+        with open(config_file) as f:
+            config = yaml.load(f)
+        if config is not None:
+            config['path'] = os.path.join(ABS_PATH, "data", name)
+            with open(config_file, "w") as f:
+                yaml.dump(config, f)
+            print(f"Updated path in yaml for {name}")
+
+outfname = "yolov5-detectors.zip"
+output = f"models/{outfname}"
+if not os.path.exists(output):
+    print("Downloading yolov5 detectors")
+    yolov5_detector_url = "https://drive.google.com/uc?id=1gfgtLgyLpYa0YLsHkpBF2YWioHR3_ilw"
+    gdown.download(yolov5_detector_url, output, quiet=False)
+    cmd=f'''
+cd models/
+unzip {outfname}
+cd ..
+'''
+    subprocess.check_output(cmd, shell=True)
+else:
+    print(f"{output} already exists")
+
+os.makedirs("data/thor", exist_ok=True)
+outfname = "corrs.zip"
+output = f"data/thor/{outfname}"
+if not os.path.exists(output):
+    print("Downloading corr distances")
+    corr_distances_url = "https://drive.google.com/uc?id=1m9E9fxc2HuX8uP4KyT3WePYi4FWFBPH3"
+    gdown.download(corr_distances_url, output, quiet=False)
+    cmd=f'''
+cd data/thor
+unzip {outfname}
+cd ../..
+'''
+    subprocess.check_output(cmd, shell=True)
+else:
+    print(f"{output} already exists")
