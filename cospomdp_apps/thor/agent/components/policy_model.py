a65c92a1a644608bb3981a216fbfa5dc08795b95
Kaiyu Zheng <kaiyutony@gmail.com>
2021-09-13 15:04:24 -0400

    this looks good to me

--- cospomdp_apps/thor/agent/components/policy_model.py
+++ cospomdp_apps/thor/agent/components/policy_model.py
@@ -66,12 +66,8 @@ class PolicyModelTopo(cospomdp.PolicyModel):
             self.num_visits_init = num_visits_init
             self.val_init = val_init
             self.policy_model = policy_model
-            self._cache = {}
 
         def get_preferred_actions(self, state, history):
-            if state in self._cache:
-                return self._cache[state]
-
             # If you have taken done before, you are done. So keep the done.
             last_action = history[-1][0] if len(history) > 0 else None
             if isinstance(last_action, Done):
@@ -109,7 +105,6 @@ class PolicyModelTopo(cospomdp.PolicyModel):
                         if zi.loc is not None:
                             preferences.add((move, self.num_visits_init, self.val_init))
                             break
-            self._cache[state] = preferences
             return preferences
 
 
