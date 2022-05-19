649bd4e05a5e22a187457a3ba4a9e691a2e1056a
Kaiyu Zheng <kaiyutony@gmail.com>
2022-05-19 17:42:36 -0400

    apply license

--- cospomdp_apps/basic/search.py
+++ cospomdp_apps/basic/search.py
@@ -1,3 +1,17 @@
+# Copyright 2022 Kaiyu Zheng
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # This is a toy domain for 2D COS-POMDP
 import pomdp_py
 from cospomdp.models.reward_model import ObjectSearchRewardModel
